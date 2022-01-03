ef898cb6a4b030b6dfe08ce4f4264f730d97476c
bhvadeepreddy <bhavadeep2003@gmail.com>
2022-01-03 10:46:50 +0530

    commit

--- index.js
+++ index.js
@@ -11,3 +11,40 @@ const message = document.getElementById("message")
 const rollBtn  = document.getElementById("rollBtn")
 const  resetBtn = document.getElementById("resetBtn")
 
+
+rollBtn.addEventListener("click",function()
+{
+ let d = Math.floor(Math.random()* 6)  + 1
+
+ if (player1Turn)
+ {
+player1Dice.textContent = d
+player1Dice.classList.remove("active")
+player2Dice.classList.add("active")
+message.textContent = "Player 2 Turn"
+player1Score += d
+player1Scoreboard.textContent = player1Score
+ }else
+ {
+    player2Dice.textContent = d
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+    message.textContent = "Player 1 Turn"
+    player2Score += d
+    player2Scoreboard.textContent = player2Score
+
+ }
+
+ if (player1Score >= 20) {
+    message.textContent = "Player 1 has won! 🥳"
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+} else if (player2Score >= 20) {
+    message.textContent = "Player 2 has won! 🎉"
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+ player1Turn = !player1Turn
+})
+
+
